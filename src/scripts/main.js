659fc6242e95bfa978172e3f9755148700137d33
Pandu Ananto <pandunih@gmail.com>
2020-05-04 23:17:36 +0700

    init layout statistic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,33 +8,6 @@ navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
 
-// const pencegahanListElement = document.querySelector("#pencegahanList-column");
-
-// pencegahanListElement.innerHTML = "";
-
-// for (let pencegahanItem of pencegahan) {
-//   let title = pencegahanItem.title;
-//   let description = pencegahanItem.description;
-//   let image = pencegahanItem.image;
-//   let alt = pencegahanItem.alt;
-
-//   let pencegahanElement = document.createElement("div");
-//   pencegahanElement.setAttribute("class", "pencegahan-row");
-
-//   let pencegahanTextElement = document.createElement("div");
-//   pencegahanTextElement.setAttribute("class", "pencegahan-text");
-
-//   pencegahanTextElement.innerHTML = "<h2>" + title + "</h2>";
-//   pencegahanTextElement.innerHTML += "<p>" + description + "</p>";
-
-//   pencegahanElement.appendChild(pencegahanTextElement);
-
-//   pencegahanElement.innerHTML +=
-//     '<img src="' + image + '" alt="' + alt + '"class="pencegahan-image">';
-
-//   pencegahanListElement.appendChild(pencegahanElement);
-// }
-
 const pencegahanListContainer = document.querySelector(
   "#pencegahan-list-container"
 );
